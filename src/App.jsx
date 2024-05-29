9d8cd3fd1e91bdc8c45062d279c3383306679b2f
KevinV86 <kv759668@gmail.com>
2024-05-28 21:35:34 -0300

    se agregaron los codigos correctos al trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,62 @@ import { Component } from 'react'
 import axios from 'axios';
 import './App.css'
 
-
-export default class App extends Component{
-  constructor(props){
+export default class App extends Component {
+  constructor (props){
     super(props);
     this.state = {
-      categorias:[]
+      categoria_id: null,
+      categorias:[],
+      productos: []
     }
   }
+
   buscarCategorias(){
     const url = "https://productos.ctpoba.edu.ar/api/categorias"
     axios.get(url)
-    .then((resp)=>{
-      this.setState({categorias: resp.data.categorias});
+    .then((resp) => {
+      this.setState({categorias: resp.data.categorias})
     })
-    .catch ((error)=>{
-      console.log(error);
+    .catch((error) => {
+      console.log(error)
     })
   }
 
-  render(){
-    return(
-      <div>
-       <span>APP</span>
-       <input
-        type="button"
-        value="buscar"
-        onAuxClick={()=> this.buscarCategorias()}
-         />
-      </div>
-
-    )
-
+    buscarProductos(){
+      const url = "https://productos.ctpoba.edu.ar/api/productos"
+      axios.get(url)
+      .then((resp) => {
+        this.setState({productos: resp.data.productos})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      
+    
   }
-};
+
+render(){
+  return (
+  <div>
+  <span>APP</span>
+  <input type="button" 
+    value="Buscar Categoria"
+    onClick={() => this.buscarCategorias()} />
+   <input type="button" 
+    value="Buscar Producto"
+    onClick={() => this.buscarProductos()} />
+  <select 
+    value={this.state.categoria_id}
+    onChange={(e)=> this.setState ({categoria_id:e.target.value})}
+  >
+    {
+      this.state.categorias.map((categoria,index)=>
+      <option key={categoria.id} value={categoria.id}>{categoria.nombre}</option>)
+    }
+  </select>
+  </div> 
+
+  )
+}
+
+}
