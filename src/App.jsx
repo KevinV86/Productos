f01f84a8e83b8971b47bd9861297e30bd4610f27
KevinV86 <kv759668@gmail.com>
2024-05-29 17:25:06 -0300

    se agregaron algunas funciones y arreglos al codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,72 @@
-import { Component } from 'react'
+import { Component } from 'react';
 import axios from 'axios';
-import './App.css'
+import './App.css';
 
 export default class App extends Component {
-  constructor (props){
+  constructor(props) {
     super(props);
     this.state = {
-      categoria_id: null,
-      categorias:[],
-      productos: []
-    }
+      categoria_id: '',
+      categorias: [],
+      productos: [],
+    };
   }
 
-  buscarCategorias(){
-    const url = "https://productos.ctpoba.edu.ar/api/categorias"
+  componentDidMount() {
+    this.buscarCategorias();
+    this.buscarProductos();
+  }
+
+  buscarCategorias = () => {
+    const url = "https://productos.ctpoba.edu.ar/api/categorias";
     axios.get(url)
-    .then((resp) => {
-      this.setState({categorias: resp.data.categorias})
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-
-    buscarProductos(){
-      const url = "https://productos.ctpoba.edu.ar/api/productos"
-      axios.get(url)
       .then((resp) => {
-        this.setState({productos: resp.data.productos})
+        this.setState({ categorias: resp.data.categorias });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
+      });
+  };
+
+  buscarProductos = () => {
+    const url = "https://productos.ctpoba.edu.ar/api/productos";
+    axios.get(url)
+      .then((resp) => {
+        this.setState({ productos: resp.data.productos });
       })
-      
-    
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <span>APP</span>
+        <input 
+          type="button" 
+          value="Buscar Categoría"
+          onClick={this.buscarCategorias} 
+        />
+        <input 
+          type="button" 
+          value="Buscar Producto"
+          onClick={this.buscarProductos} 
+        />
+        <select 
+          value={this.state.categoria_id}
+          onChange={(e) => this.setState({ categoria_id: e.target.value })}
+        >
+          <option value="">Seleccione una categoría</option>
+          {
+            this.state.categorias.map((categoria) => (
+              <option key={categoria.id} value={categoria.id}>
+                {categoria.nombre}
+              </option>
+            ))
+          }
+        </select>
+      </div>
+    );
   }
-
-render(){
-  return (
-  <div>
-  <span>APP</span>
-  <input type="button" 
-    value="Buscar Categoria"
-    onClick={() => this.buscarCategorias()} />
-   <input type="button" 
-    value="Buscar Producto"
-    onClick={() => this.buscarProductos()} />
-  <select 
-    value={this.state.categoria_id}
-    onChange={(e)=> this.setState ({categoria_id:e.target.value})}
-  >
-    {
-      this.state.categorias.map((categoria,index)=>
-      <option key={categoria.id} value={categoria.id}>{categoria.nombre}</option>)
-    }
-  </select>
-  
-  </div> 
-
-  )
-}
-
 }
